fc287a6e8c51856b6b2a3e7e60129aae4b273bd3
SarSima <andreisimazyalin@yandex.ru>
2024-07-31 23:55:17 +0300

    Add new files

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:id>', views.post_detail, name='detail'),
+    path('posts/<int:id>/', views.post_detail, name='detail'),
     path('category/<slug:category_slug>/',
          views.category_posts,
          name='category')
